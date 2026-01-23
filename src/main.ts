f69fd44f7b66da504f2960e9f38f22634f656d5b
Kseniia_Liepokhina <76071580+KseniiaLepiohina@users.noreply.github.com>
2026-01-23 17:54:47 +0200

    add the mailer autosending logic and swagger documentation (#6)

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 // import { AuthGuard } from './guards/jwt.payload';
 // import { JwtService } from '@nestjs/jwt';
 // import { createTestAccount } from 'nodemailer';
@@ -9,6 +10,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Tours to Tuscany')
+    .setDescription('The Tours to Tuscany API description')
+    .setVersion('1.0')
+    .addTag('Tuscany')
+    .build()
+    const  documentFactory  = () => SwaggerModule.createDocument(app,config);
+    SwaggerModule.setup('api', app, documentFactory);
   app.enableCors({
     origin:'tourstotuscany-frontend.vercel.app',
     methods:'GET,HEAD,PUT,PATCH,POST,DELETE',
