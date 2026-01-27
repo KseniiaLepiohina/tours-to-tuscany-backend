c47223ec4f1a0c48e1c1c08459e86f1a45978197
Kseniia_Liepokhina <76071580+KseniiaLepiohina@users.noreply.github.com>
2026-01-27 22:30:42 +0200

    database module (#7)
    Added a separeted module for connection remote DB

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-// import { AuthGuard } from './guards/jwt.payload';
-// import { JwtService } from '@nestjs/jwt';
-// import { createTestAccount } from 'nodemailer';
-// import * as serviceAccount from '../src/firebase-adminsdk.json'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -24,13 +20,7 @@ async function bootstrap() {
     credentials:true,
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-
-  // const jwtService = app.get(JwtService);
-  // app.useGlobalGuards(new AuthGuard(jwtService));
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-// });
-  await app.listen(5000);
+  await app.listen(process.env.PORT || 3000);
 
 }
 bootstrap();
